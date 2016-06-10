728a73f9c142af3b6e41ebae80e72a8f3fc840b1
kzkkzk <kzk_kzk@rambler.ru>
2016-06-10 14:07:58 +0300

    Изменено позиционирование нижней трапеции

--- js/cds.js
+++ js/cds.js
@@ -1,10 +1,16 @@
-var blc_element = $(".blc");
-var tlc_element = $(".tlc");
-
 var resize_blc = function(){
-    blc_element.css( 'border-width', $('.GBottom').height() + 'px');
-    blc_element.css('width', tlc_element.width() + 114 + 'px');
+    var gbottom_element = $('.GBottom');
+    var blc_element = $(".blc");
+    var tlc_element = $(".tlc");
+
+    var gbottom_height= gbottom_element.height();                                   /* Высота нижнего div'a */
+
+    blc_element.css( 'border-width', gbottom_height + 'px');
+    blc_element.css( 'width', gbottom_element.width()*75/100 - gbottom_height + 'px');
+
 };
 
 $(window).load( resize_blc);
 $(window).resize( resize_blc);
+
+
