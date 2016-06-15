affeca2667967e5c00f1134aa06ce58daa6cfdf9
kzkkzk <kzk_kzk@rambler.ru>
2016-06-16 00:27:30 +0300

    Пляски с верхним элементом

--- js/cds.js
+++ js/cds.js
@@ -13,18 +13,12 @@ var resize_blc = function(){
     var d_main_diamond = content_element.height()- (diamond_element.css( 'border-width').replace('px', '') * 2);  /*Диагональ ромба*/
     var w_main_diamond = d_main_diamond / Math.sqrt(2);                                                           /*Сторона ромба*/
 
-    /*   var gtop_height   = gtop_element.height();                                   /* Высота верхнего div'a */
-    /*   var gbottom_height= gbottom_element.height();                                /* Высота нижнего div'a */
- /*   tlc_element.css( 'border-width', gtop_height    + 'px');
-    blc_element.css( 'border-width', gbottom_height + 'px');
-
-    blc_element.css( 'width', gbottom_element.width()*75/100 - gbottom_height + 'px');
-    tlc_element.css( 'width', blc_element.width() -  gtop_height + 'px'); */
-
     diamond_element.css( {'width': w_main_diamond, 'height': w_main_diamond});
 
-    var mt = (content_element.height() - w_main_diamond) /2 - 17;
+    var mt = (content_element.height() - w_main_diamond) /2 - 17; /*Значение margin-top для ромба*/
     diamond_element.css( 'margin-top',  mt + 'px');
+
+    gtop_element.css('height', diamond_element.width()/ 2 - 30 + 'px');
 
     $('.info').html( 'bone - ' + $('.bone').height() +
         '<br>Content- ' + content_element.height() +
@@ -32,6 +26,26 @@ var resize_blc = function(){
         '<br>d_diamond- ' + d_main_diamond +
         '<br>w_diamond- ' + w_main_diamond +
         '<br>diamond_border- ' + diamond_element.css( 'border-width'));
+
+    var gtop_height   = gtop_element.height();                                   /* Высота верхнего div'a */
+    /*   var gbottom_height= gbottom_element.height();                                /* Высота нижнего div'a */
+    tlc_element.css( 'border-width', gtop_height    + 'px');
+
+    var tlc_delta = (Math.sqrt( Math.pow(w_main_diamond / 2, 2) - Math.pow(gtop_height, 2))+ 60);
+
+
+
+    tlc_element.css( 'width', (gtop_element.width()/2 - gtop_height) + 'px');
+    
+
+
+ /*   tlc_element.css( 'width', (gtop_element.width()/2 - gtop_height) - (Math.sqrt( Math.pow(w_main_diamond / 2, 2) - Math.pow(gtop_height, 2))) - 60 + 'px');
+
+
+    /* blc_element.css( 'border-width', gbottom_height + 'px');
+
+     blc_element.css( 'width', gbottom_element.width()*75/100 - gbottom_height + 'px');
+      */
 
     /*   diamond_element.css( {'left': (d_main_diamond - w_main_diamond) /2 + 12, 'top': (d_main_diamond - w_main_diamond) /2 + 12}); */
     /*    var mt = d_main_diamond - w_main_diamond - diamond_element.css( 'border-width').replace('px', '') * 2 - 4;
